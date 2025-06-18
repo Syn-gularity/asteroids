2ba94c8a5119c2a4739e7be31de219590ee8817a
Syn-gularity <jan_hendrik_ernst@hotmail.com>
2025-06-19 00:03:15 +0200

    fixed the splitting

--- asteroid.py
+++ asteroid.py
@@ -14,9 +14,19 @@ class Asteroid(CircleShape):
         self.position += self.velocity * dt
 
     def split(self):
+        
+
         if self.radius <= ASTEROID_MIN_RADIUS:
             self.kill()
-        elif
+        else:
+            angle = random.uniform(20,50)
+            right = self.velocity.rotate(angle)
+            left = self.velocity.rotate(-angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            left_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            left_asteroid.velocity = left * 1.2
+            right_asteroid = Asteroid(self.position.x, self.position.y, new_radius)
+            right_asteroid.velocity = right * 1.2
         
         self.kill()
         
