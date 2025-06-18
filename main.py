890e64994b63703b9765d840831f4041f5e3d077
Syn-gularity <jan_hendrik_ernst@hotmail.com>
2025-06-18 21:49:55 +0200

    add circleshape

--- main.py
+++ main.py
@@ -5,12 +5,18 @@ import pygame
 from constants import *
 
 def main():
-    pygame.init
+    pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # Creates new screen with given parameters
+    py_clock = pygame.time.Clock() # Initialize a Clock for clockspeed
+    dt = 0
+
     while True:
         # Check for PlayerInput
         # Update World
         # Draw new state
+
+        dt = py_clock.tick(60)
+        dt /= 1000
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
